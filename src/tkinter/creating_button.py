6acea9c8d555a3186697c84d04705763acaa5b0b
MatthewNewell006 <matthewnewell006@gmail.com>
2021-01-27 20:22:40 -0500

    Update

---
+++ src/tkinter/creating_button.py
@@ -0,0 +1,17 @@
+from tkinter import *
+
+root = Tk()
+root.geometry('100x100')
+
+btn = Button(root, text = 'Click me!', bd = '5', command = root.destroy)
+btn.pack(side = 'top')
+
+root.mainloop()
+
+
+# =============================================================================================================
+
+from tkinter import *
+from tkinter.ttk import *
+
+root = Tk()
